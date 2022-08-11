574b06969b47ca991ce4b74020b5d475acb4ef2f
Carl Gudmundsson <komoss@gmail.com>
2022-08-11 23:18:35 +0200

    Add basic tests

--- main.rb
+++ main.rb
@@ -2,3 +2,30 @@
 
 require_relative 'lib/linked_list'
 require_relative 'lib/node'
+
+list = LinkedList.new
+
+list.append("Test1")
+list.append("Test2")
+
+puts list.contains?("Test1")
+puts list.contains?("Test2")
+puts list.contains?("Test3")
+
+p list.find("Test1")
+p list.find("Test2")
+p list.find("Test3")
+
+puts list.head
+puts list.tail
+puts list.size
+
+list.prepend("Test0")
+puts list
+
+puts list.at(1)
+list.insert_at("insert", 1)
+puts list
+
+list.remove_at(1)
+puts list
