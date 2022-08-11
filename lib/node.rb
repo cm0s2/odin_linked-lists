487801b5c8c2eee5477f634cf37885ec165c0ba1
Carl Gudmundsson <komoss@gmail.com>
2022-08-11 14:43:55 +0200

    Change name of next to next_node

--- lib/node.rb
+++ lib/node.rb
@@ -2,10 +2,10 @@
 
 class Node
   attr_reader :value
-  attr_accessor :next
+  attr_accessor :next_node
 
-  def initialize(value, next=nil)
+  def initialize(value, next_node=nil)
     @value = value
-    @next = next
+    @next_node = next_node
   end
 end
